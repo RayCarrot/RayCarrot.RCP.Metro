427868baf36e05f94160817d2cc5fdaddb582b51
RayCarrot <RayCarrotMaster@gmail.com>
2019-01-30 13:52:41 +0100

    R2 widescreen support WIP

--- RayCarrot.RCP.Metro/Controls/UserControls/Config/Rayman2Config.xaml.cs
+++ RayCarrot.RCP.Metro/Controls/UserControls/Config/Rayman2Config.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Nito.AsyncEx;
 using RayCarrot.CarrotFramework;
 using RayCarrot.Rayman;
 
@@ -31,7 +33,116 @@ namespace RayCarrot.RCP.Metro
         public Rayman2ConfigViewModel()
         {
             IsHorizontalWidescreen = true;
+
+            AsyncLock = new AsyncLock();
+
+            SaveCommand = new AsyncRelayCommand(SaveAsync);
         }
+
+        #endregion
+
+        #region Private Fields
+
+        private int _resX;
+
+        private int _resY;
+
+        private bool _lockToScreenRes;
+
+        private bool _widescreenSupport;
+
+        #endregion
+
+        #region Private Properties
+
+        private AsyncLock AsyncLock { get; }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The configuration data
+        /// </summary>
+        public R2UbiIniHandler ConfigData { get; set; }
+
+        /// <summary>
+        /// The ubi.ini config file path
+        /// </summary>
+        public FileSystemPath ConfigPath { get; set; }
+
+        /// <summary>
+        /// The current horizontal resolution
+        /// </summary>
+        public int ResX
+        {
+            get => _resX;
+            set
+            {
+                _resX = value;
+                UnsavedChanges = true;
+            }
+        }
+
+        /// <summary>
+        /// The current vertical resolution
+        /// </summary>
+        public int ResY
+        {
+            get => _resY;
+            set
+            {
+                _resY = value;
+                UnsavedChanges = true;
+            }
+        }
+
+        /// <summary>
+        /// Indicates if widescreen support is enabled
+        /// </summary>
+        public bool WidescreenSupport
+        {
+            get => _widescreenSupport;
+            set
+            {
+                _widescreenSupport = value;
+
+                if (value && LockToScreenRes)
+                    ResX = (int)SystemParameters.PrimaryScreenWidth;
+            }
+        }
+
+        /// <summary>
+        /// Indicates if the resolution is locked to the current screen resolution
+        /// </summary>
+        public bool LockToScreenRes
+        {
+            get => _lockToScreenRes;
+            set
+            {
+                _lockToScreenRes = value;
+
+                if (!value)
+                    return;
+
+                ResY = (int)SystemParameters.PrimaryScreenHeight;
+
+                ResX = WidescreenSupport
+                    ? (int) SystemParameters.PrimaryScreenWidth
+                    : (int) Math.Round((double) ResY / 3 * 4);
+            }
+        }
+
+        /// <summary>
+        /// Indicates if the widescreen support is horizontal, otherwise it is vertical
+        /// </summary>
+        public bool IsHorizontalWidescreen { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public AsyncRelayCommand SaveCommand { get; }
 
         #endregion
 
@@ -80,81 +191,167 @@ namespace RayCarrot.RCP.Metro
 
             ResX = ConfigData.FormattedGLI_Mode.ResX;
             ResY = ConfigData.FormattedGLI_Mode.ResY;
+
+            UnsavedChanges = false;
         }
 
-        #endregion
-
-        #region Private Fields
-
-        private bool _lockToScreenRes;
-
-        private bool _widescreenSupport;
-
-        #endregion
-
-        #region Public Properties
-
         /// <summary>
-        /// The configuration data
+        /// Saves the changes
         /// </summary>
-        public R2UbiIniHandler ConfigData { get; set; }
-
-        /// <summary>
-        /// The ubi.ini config file path
-        /// </summary>
-        public FileSystemPath ConfigPath { get; set; }
-
-        /// <summary>
-        /// The current horizontal resolution
-        /// </summary>
-        public int ResX { get; set; }
-
-        /// <summary>
-        /// The current vertical resolution
-        /// </summary>
-        public int ResY { get; set; }
-
-        /// <summary>
-        /// Indicates if widescreen support is enabled
-        /// </summary>
-        public bool WidescreenSupport
+        /// <returns>The task</returns>
+        public async Task SaveAsync()
         {
-            get => _widescreenSupport;
-            set
+            using (await AsyncLock.LockAsync())
             {
-                _widescreenSupport = value;
+                try
+                {
+                    ConfigData.GLI_Mode = new RayGLI_Mode()
+                    {
+                        ColorMode = ConfigData.FormattedGLI_Mode?.ColorMode ?? 16,
+                        IsWindowed = ConfigData.FormattedGLI_Mode?.IsWindowed ?? false,
+                        ResX = ResX,
+                        ResY = ResY
+                    }.ToString();
 
-                if (value && LockToScreenRes)
-                    ResX = (int)SystemParameters.PrimaryScreenWidth;
+                    await SetAspectRatioAsync();
+                    // TODO: If widescreen support is true, edit .exe file - if false, edit to make sure it's removed from previous sessions
+
+                    ConfigData.Save();
+
+                    UnsavedChanges = false;
+                }
+                catch (Exception ex)
+                {
+                    ex.HandleError("Saving R2 ubi.ini data");
+                    // TODO: Handle error
+                }
             }
         }
 
+        #endregion
+
+        #region Private Methods
+
         /// <summary>
-        /// Indicates if the resolution is locked to the current screen resolution
+        /// Sets the aspect ratio for the Rayman 2 executable file
         /// </summary>
-        public bool LockToScreenRes
+        /// <returns>The task</returns>
+        private async Task SetAspectRatioAsync()
         {
-            get => _lockToScreenRes;
-            set
+            try
             {
-                _lockToScreenRes = value;
+                // Get the file path
+                FileSystemPath path = Games.Rayman2.GetLaunchInfo().Path;
 
-                if (!value)
+                // Make sure the file exists
+                if (!path.FileExists)
+                {
+                    // TODO: Fill out
+
+                    if (WidescreenSupport)
+                        await RCF.MessageUI.DisplayMessageAsync("", "Error", MessageType.Error);
+                    else
+                        RCF.Logger.LogWarningSource("");
+
                     return;
+                }
 
-                ResY = (int)SystemParameters.PrimaryScreenHeight;
+                // Get the size to determine the version
+                var length = path.GetSize();
+
+                // Get the byte location
+                int location = 0;
+
+                // Check if it's the disc version
+                if ((int)length.Bytes == 676352)
+                    location = 633496;
+
+                // Check if it's the GOG version
+                else if ((int)length.Bytes == 1468928)
+                    location = 640152;
+
+                // Cancel if unknown version
+                if (location == 0)
+                {
+                    // TODO: Fill out
+
+                    if (WidescreenSupport)
+                        await RCF.MessageUI.DisplayMessageAsync("", "Error", MessageType.Error);
+                    else
+                        RCF.Logger.LogWarningSource("");
+
+                    return;
+                }
+
+                // Apply widescreen patch
+                if (WidescreenSupport)
+                {
+                    // Get the aspect ratio
+                    float ratio = (float)ResY / ResX;
+
+                    // Multiply by 4/3
+                    ratio = ratio * (4.0F / 3.0F);
+
+                    // Get the hex representation of the value
+                    string value = Hex.GetHexRepresentation(ratio);
+
+                    // Get the byte array to write
+                    byte[] input = Enumerable.Range(0, value.Length)
+                        .Where(x => x % 2 == 0)
+                        .Select(x => Convert.ToByte(value.Substring(x, 2), 16))
+                        .ToArray();
+
+                    // Open the file
+                    using (Stream stream = File.Open(path, FileMode.Open))
+                    {
+                        // Set the position
+                        stream.Position = location;
+
+                        // Write the bytes
+                        await stream.WriteAsync(input, 0, input.Length);
+                    }
+                }
+                // Restore to 4/3 if modified previously
+                else
+                {
+                    // Open the file
+                    using (Stream stream = File.Open(path, FileMode.Open))
+                    {
+                        // Set the position
+                        stream.Position = location;
+
+                        // Create the buffer
+                        var buffer = new byte[4];
+
+                        // Read the bytes
+                        await stream.ReadAsync(buffer, 0, 4);
+
+                        // Check if the data has been modified
+                        if (buffer[0] != 0 || buffer[1] != 0 || buffer[2] != 128 || buffer[3] != 63)
+                        {
+                            // Set the position
+                            stream.Position = location;
+
+                            // Write the bytes
+                            await stream.WriteAsync(new byte[]
+                            {
+                                0,
+                                0,
+                                128,
+                                63
+                            }, 0, 4);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: Fill out
 
                 if (WidescreenSupport)
-                    ResX = (int)SystemParameters.PrimaryScreenWidth;
-                else
-                    ResX = (int)Math.Round(((double)ResY / 3) * 4);
+                    throw;
             }
         }
-
-        /// <summary>
-        /// Indicates if the widescreen support is horizontal, otherwise it is vertical
-        /// </summary>
-        public bool IsHorizontalWidescreen { get; set; }
 
         #endregion
 
