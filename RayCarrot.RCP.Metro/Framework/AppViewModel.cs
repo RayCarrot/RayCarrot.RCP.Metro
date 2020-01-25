fa0418d5b590bc03c66258cd0c8d785fc9739329
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-01-25 12:36:53 +0100

    Version 9.1.0

--- RayCarrot.RCP.Metro/Framework/AppViewModel.cs
+++ RayCarrot.RCP.Metro/Framework/AppViewModel.cs
@@ -126,7 +126,7 @@ namespace RayCarrot.RCP.Metro
         /// <summary>
         /// The current app version
         /// </summary>
-        public Version CurrentAppVersion => new Version(9, 0, 0, 5);
+        public Version CurrentAppVersion => new Version(9, 1, 0, 0);
 
         /// <summary>
         /// Indicates if the current version is a beta version
