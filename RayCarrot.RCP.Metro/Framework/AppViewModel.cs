95eac963af0467fe2924f3b839456d41484f995b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-02-27 16:04:41 +0100

    Added WIP Rayman 1 level editor

--- RayCarrot.RCP.Metro/Framework/AppViewModel.cs
+++ RayCarrot.RCP.Metro/Framework/AppViewModel.cs
@@ -126,7 +126,7 @@ namespace RayCarrot.RCP.Metro
         /// <summary>
         /// The current app version
         /// </summary>
-        public Version CurrentAppVersion => new Version(9, 4, 0, 2);
+        public Version CurrentAppVersion => new Version(9, 4, 0, 3);
 
         /// <summary>
         /// Indicates if the current version is a beta version
