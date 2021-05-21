ac46a2e0485663b8b6dec54ad3ad1c3a0ff10aeb
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-05-21 18:04:26 +0200

    Prepare version 11.2.0

--- RayCarrot.RCP.Metro/App/AppViewModel.cs
+++ RayCarrot.RCP.Metro/App/AppViewModel.cs
@@ -142,12 +142,12 @@ namespace RayCarrot.RCP.Metro
         /// <summary>
         /// The current app version
         /// </summary>
-        public Version CurrentAppVersion => new Version(11, 1, 0, 1);
+        public Version CurrentAppVersion => new Version(11, 2, 0, 0);
 
         /// <summary>
         /// Indicates if the current version is a beta version
         /// </summary>
-        public bool IsBeta => false;
+        public bool IsBeta => true;
 
         /// <summary>
         /// Shortcut to the app user data
