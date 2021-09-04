94edc2179d04b555da0b15e4f173b1de525b42ac
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-09-04 08:45:18 +0200

    Improved scroll viewer layout in help page

--- RayCarrot.RCP.Metro/Pages/Help/Page_Help_UI.xaml.cs
+++ RayCarrot.RCP.Metro/Pages/Help/Page_Help_UI.xaml.cs
@@ -18,11 +18,5 @@ namespace RayCarrot.RCP.Metro
                     ((TreeViewItem)HelpTreeView.ItemContainerGenerator.ContainerFromItem(HelpTreeView.Items[0])).IsSelected = true;
             };
         }
-
-        private void UIElement_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            // Redirect vertical scrolling to parent scroll viewer
-            MainScrollViewer.ScrollToVerticalOffset(MainScrollViewer.VerticalOffset + (e.Delta * -0.5));
-        }
     }
 }
