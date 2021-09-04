eadf488658755f4a56978b2ec7b71292800208a3
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-09-04 08:21:25 +0200

    Made tab transition type be dynamic based on the selection

---
+++ RayCarrot.RCP.Metro/UI/AttachableForStyleBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace RayCarrot.RCP.Metro
+{
+    public abstract class AttachableForStyleBehavior<TComponent, TBehavior> : Behavior<TComponent>
+        where TComponent : DependencyObject
+        where TBehavior : AttachableForStyleBehavior<TComponent, TBehavior>, new()
+    {
+        public bool IsEnabledForStyle
+        {
+            get => (bool)GetValue(IsEnabledForStyleProperty);
+            set => SetValue(IsEnabledForStyleProperty, value);
+        }
+
+        public static DependencyProperty IsEnabledForStyleProperty =
+            DependencyProperty.RegisterAttached(nameof(IsEnabledForStyle), typeof(bool),
+            typeof(AttachableForStyleBehavior<TComponent, TBehavior>), new FrameworkPropertyMetadata(false, OnIsEnabledForStyleChanged));
+
+        private static void OnIsEnabledForStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not UIElement uie) 
+                return;
+            
+            BehaviorCollection behColl = Interaction.GetBehaviors(uie);
+            var existingBehavior = behColl.FirstOrDefault(b => b.GetType() == typeof(TBehavior)) as TBehavior;
+
+            if ((bool)e.NewValue == false && existingBehavior != null)
+                behColl.Remove(existingBehavior);
+
+            else if ((bool)e.NewValue && existingBehavior == null)
+                behColl.Add(new TBehavior());
+        }
+    }
+}
---
+++ RayCarrot.RCP.Metro/UI/Behaviors/DynamicTabAnimationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+using MahApps.Metro.Controls;
+
+namespace RayCarrot.RCP.Metro
+{
+    public class DynamicTabAnimationBehavior : AttachableForStyleBehavior<MetroAnimatedTabControl, DynamicTabAnimationBehavior>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+        }
+
+        private int prevSelectedIndex;
+
+        private void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AssociatedObject.SelectedIndex == -1) 
+                return;
+
+            if (RCPServices.Data.EnableAnimations)
+                TabControlHelper.SetTransition(AssociatedObject,
+                    prevSelectedIndex > AssociatedObject.SelectedIndex ? TransitionType.Right : TransitionType.Left);
+            else
+                TabControlHelper.SetTransition(AssociatedObject, TransitionType.Normal);
+
+            prevSelectedIndex = AssociatedObject.SelectedIndex;
+        }
+    }
+}
