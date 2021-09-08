4cbabdcd3447b8a3095ccfe7fa20b9e729844a90
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-09-08 13:16:15 +0200

    Update browse box binding when the enter key is pressed

---
+++ RayCarrot.RCP.Metro/UI/Behaviors/UpdateTextBoxBindingOnEnterBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RayCarrot.RCP.Metro
+{
+    public class UpdateTextBoxBindingOnEnterBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.KeyDown += AssociatedObject_KeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.KeyDown -= AssociatedObject_KeyDown;
+        }
+
+        private void AssociatedObject_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                AssociatedObject.GetBindingExpression(TextBox.TextProperty)?.UpdateSource();
+        }
+    }
+}
