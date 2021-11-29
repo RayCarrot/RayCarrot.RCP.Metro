438cb695813865b31956957a19bceadf15d929e8
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-29 16:25:43 +0100

    Normalize buttons with icons using data template

---
+++ src/RayCarrot.RCP.Metro/UI/AttachedProperties/ButtonAssist.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.IconPacks;
+
+namespace RayCarrot.RCP.Metro;
+
+public static class ButtonAssist
+{
+    #region Icon Kind
+
+    public static PackIconMaterialKind GetIconKind(Button obj) => (PackIconMaterialKind)obj.GetValue(IconKindProperty);
+
+    public static void SetIconKind(Button obj, PackIconMaterialKind value) => obj.SetValue(IconKindProperty, value);
+
+    public static readonly DependencyProperty IconKindProperty = DependencyProperty.RegisterAttached(
+        name: "IconKind",
+        propertyType: typeof(PackIconMaterialKind),
+        ownerType: typeof(ButtonAssist));
+
+    #endregion
+}
