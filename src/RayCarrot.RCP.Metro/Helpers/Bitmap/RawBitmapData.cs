d159cd80a56ed7b4220bb11848dd9a7a0b691a9e
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-29 17:00:15 +0100

    Move over bitmap helpers from RayCarrot.Rayman

---
+++ src/RayCarrot.RCP.Metro/Helpers/Bitmap/RawBitmapData.cs
@@ -0,0 +1,76 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace RayCarrot.RCP.Metro;
+
+/// <summary>
+/// Raw image data
+/// </summary>
+public class RawBitmapData
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="RawBitmapData"/> from a <see cref="Bitmap"/>
+    /// </summary>
+    /// <param name="bmp">The bitmap to get the raw data from</param>
+    public RawBitmapData(Bitmap bmp)
+    {
+        using var bmpLock = new BitmapLock(bmp);
+
+        Width = bmp.Width;
+        Height = bmp.Height;
+        PixelData = bmpLock.Pixels;
+        PixelFormat = bmp.PixelFormat;
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="RawBitmapData"/> from specified values
+    /// </summary>
+    /// <param name="width">The image width</param>
+    /// <param name="height">The image height</param>
+    /// <param name="pixelData">The pixel data</param>
+    /// <param name="pixelFormat">The pixel format</param>
+    public RawBitmapData(int width, int height, byte[] pixelData, PixelFormat pixelFormat)
+    {
+        Width = width;
+        Height = height;
+        PixelData = pixelData;
+        PixelFormat = pixelFormat;
+    }
+
+    /// <summary>
+    /// The image width
+    /// </summary>
+    public int Width { get; }
+
+    /// <summary>
+    /// The image height
+    /// </summary>
+    public int Height { get; }
+
+    /// <summary>
+    /// The pixel data
+    /// </summary>
+    public byte[] PixelData { get; }
+
+    /// <summary>
+    /// The format of the pixel data
+    /// </summary>
+    public PixelFormat PixelFormat { get; }
+
+    /// <summary>
+    /// Gets a bitmap from the raw image data
+    /// </summary>
+    /// <returns>The bitmap</returns>
+    public Bitmap GetBitmap()
+    {
+        // Create the bitmap
+        Bitmap bmp = new Bitmap(Width, Height, PixelFormat);
+
+        // Lock and update the pixels
+        using (var bmpLock = new BitmapLock(bmp))
+            bmpLock.Pixels = PixelData;
+
+        // Return the bitmap
+        return bmp;
+    }
+}
---
+++ src/RayCarrot.RCP.Metro/Helpers/Extensions/ImageExtensions.cs
@@ -0,0 +1,57 @@
+﻿#nullable disable
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace RayCarrot.RCP.Metro;
+
+/// <summary>
+/// Extension methods for <see cref="Image"/>
+/// </summary>
+public static class ImageExtensions
+{
+    /// <summary>
+    /// Resizes the image using high quality bilinear interpolation
+    /// </summary>
+    /// <param name="image">The image to resize</param>
+    /// <param name="width">The width to resize to</param>
+    /// <param name="height">The height to resize to</param>
+    /// <param name="disposeImage">Indicates if the image should be disposed</param>
+    /// <returns>The resized image</returns>
+    public static Bitmap ResizeImage(this Image image, int width, int height, bool disposeImage = true)
+    {
+        try
+        {
+            // Create the frame
+            var destRect = new Rectangle(0, 0, width, height);
+
+            // Crete the image to output
+            var destImage = new Bitmap(width, height);
+
+            // Set the resolution
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            // Create a graphic
+            using var graphics = Graphics.FromImage(destImage);
+
+            // Set properties
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            using var wrapMode = new ImageAttributes();
+
+            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+            graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+
+            return destImage;
+        }
+        finally
+        {
+            if (disposeImage)
+                image?.Dispose();
+        }
+    }
+}
