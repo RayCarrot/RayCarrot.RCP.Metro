6579a5072dd02ff0de6410e3e0177ac7255aca18
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-12-20 14:11:47 +0100

    Move backup slots and info into new expander

---
+++ src/RayCarrot.RCP.Metro/UI/Converters/Visibility/BooleanOrToVisibilityMultiConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RayCarrot.RCP.Metro;
+
+public class BooleanOrToVisibilityMultiConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        return values.Any(x => x is true) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
