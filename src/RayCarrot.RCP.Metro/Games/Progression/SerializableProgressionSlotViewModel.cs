49c8184904a2ddb3426872e05a79f5fe9c4e8e6a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-01-13 16:16:03 +0100

    Comment out writing saves to temp first

--- src/RayCarrot.RCP.Metro/Games/Progression/SerializableProgressionSlotViewModel.cs
+++ src/RayCarrot.RCP.Metro/Games/Progression/SerializableProgressionSlotViewModel.cs
@@ -63,16 +63,20 @@ public class SerializableProgressionSlotViewModel<FileObj> : ProgressionSlotView
             data = JsonHelpers.DeserializeFromFile<FileObj>(filePath);
         }
 
-        // Write to a temp file first to avoid corrupting the file in case there is an error during the serialization
-        using var tempFile = new TempFile(false);
+        // TODO: Find solution
+        // Commented out first writing to temp due to the file moving at the end not keeping file attributes which causes
+        // saves for packages games not to load (for example Jungle Run)
 
-        var file = (PhysicalFile)Context.GetFile(FileName);
-        file.DestinationPath = tempFile.TempPath;
+        // Write to a temp file first to avoid corrupting the file in case there is an error during the serialization
+        //using var tempFile = new TempFile(false);
+
+        //var file = (PhysicalFile)Context.GetFile(FileName);
+        //file.DestinationPath = tempFile.TempPath;
 
         using (Context)
             FileFactory.Write<FileObj>(Context, FileName, data);
 
         // Replace the original file with the temp file
-        Services.File.MoveFile(tempFile.TempPath, file.SourcePath, true);
+        //Services.File.MoveFile(tempFile.TempPath, file.SourcePath, true);
     }
 }
