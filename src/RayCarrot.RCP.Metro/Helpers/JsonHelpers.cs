67dddbf167b931c210413b870394e00df81ce79b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-02-09 08:27:48 +0100

    Add JSON helpers for using streams

--- src/RayCarrot.RCP.Metro/Helpers/JsonHelpers.cs
+++ src/RayCarrot.RCP.Metro/Helpers/JsonHelpers.cs
@@ -34,6 +34,26 @@ public static class JsonHelpers
     }
 
     /// <summary>
+    /// Serializes an object to a stream
+    /// </summary>
+    /// <typeparam name="T">The type of file to serialize</typeparam>
+    /// <param name="obj">The object to serialize</param>
+    /// <param name="stream">The stream to serialize to</param>
+    /// <param name="converters">Optional converters to use</param>
+    public static void SerializeToStream<T>(T obj, Stream stream, params JsonConverter[] converters)
+    {
+        JsonSerializer serializer = new();
+
+        foreach (JsonConverter c in converters.Concat(GetDefaultConverters()))
+            serializer.Converters.Add(c);
+
+        using StreamWriter sr = new(stream);
+        using JsonTextWriter jsonTextWriter = new(sr);
+
+        serializer.Serialize(jsonTextWriter, obj);
+    }
+
+    /// <summary>
     /// Deserializes an object from a file
     /// </summary>
     /// <typeparam name="T">The type of object to deserialize</typeparam>
@@ -63,5 +83,25 @@ public static class JsonHelpers
 
         // Return the deserialized object
         return JsonConvert.DeserializeObject(json, type, converters.Concat(GetDefaultConverters()).ToArray());
+    }
+
+    /// <summary>
+    /// Deserializes an object from a stream
+    /// </summary>
+    /// <typeparam name="T">The type of object to deserialize</typeparam>
+    /// <param name="stream">The stream to deserialize</param>
+    /// <param name="converters">Optional converters to use</param>
+    /// <returns>The deserialized object</returns>
+    public static T DeserializeFromStream<T>(Stream stream, params JsonConverter[] converters)
+    {
+        JsonSerializer serializer = new();
+
+        foreach (JsonConverter c in converters.Concat(GetDefaultConverters()))
+            serializer.Converters.Add(c);
+
+        using StreamReader sr = new(stream);
+        using JsonTextReader jsonTextReader = new(sr);
+        
+        return serializer.Deserialize<T>(jsonTextReader);
     }
 }
