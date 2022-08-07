8d9993965129007641abe750d9b507e085fbfafe
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-08-07 13:23:02 +0200

    Download patches from URI launch

--- src/RayCarrot.RCP.Metro/App/Startup/LaunchHandler/URI/PatchFileURILaunchHandler.cs
+++ src/RayCarrot.RCP.Metro/App/Startup/LaunchHandler/URI/PatchFileURILaunchHandler.cs
@@ -1,17 +1,50 @@
-﻿using RayCarrot.RCP.Metro.Patcher;
+﻿using System;
+using System.IO;
+using NLog;
+using RayCarrot.RCP.Metro.Patcher;
 
 namespace RayCarrot.RCP.Metro;
 
 public class PatchFileURILaunchHandler : URILaunchHandler
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
     public override bool DisableFullStartup => true;
 
     public override string BaseURI => PatchFile.URIProtocol;
 
-    public override void Invoke(string uri, State state)
+    public override async void Invoke(string uri, State state)
     {
         string value = GetValue(uri);
+        
+        if (!Uri.TryCreate(value, UriKind.Absolute, out Uri patchUri))
+        {
+            Logger.Warn("URI value '{0}' was not correctly formatted", value);
+            return;
+        }
 
-        // TODO-UPDATE: Implement
+        try
+        {
+            using TempDirectory tempDir = new(true);
+
+            // Download the patch
+            bool result = await Services.App.DownloadAsync(new[] { patchUri }, false, tempDir.TempPath);
+
+            if (!result)
+                return;
+
+            // Due to how the downloading system currently works we need to get the path like this
+            FileSystemPath patchFilePath = tempDir.TempPath + Path.GetFileName(patchUri.AbsoluteUri);
+
+            // Show the Patcher
+            await Services.UI.ShowPatcherAsync(patchFilePath);
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Runing Patcher from URI launch");
+
+            // TODO-UPDATE: Localize
+            await Services.MessageUI.DisplayExceptionMessageAsync(ex, "An error occurred in the patcher and it had to close");
+        }
     }
 }
