472460a4608cd4872b36b039ef2e09dffc8ad74e
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-10-12 17:34:58 +0200

    Add copy path context menu options in archive explorer

---
+++ src/RayCarrot.RCP.Metro/UI/CommonCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RayCarrot.RCP.Metro;
+
+public static class CommonCommands
+{
+    private static ICommand? _copyToClipboardCommand;
+    public static ICommand CopyToClipboardCommand => _copyToClipboardCommand ??= new RelayCommand(x =>
+    {
+        if (x == null)
+            return;
+
+        Clipboard.SetText(x.ToString());
+    });
+}
