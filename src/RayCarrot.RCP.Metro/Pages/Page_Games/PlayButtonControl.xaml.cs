1982f78a2f444d6973978f34e0a1f22ea06b3b2d
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-11-25 16:20:45 +0100

    Modify play button drop-down design, move logic to reusable behavior

--- src/RayCarrot.RCP.Metro/Pages/Page_Games/PlayButtonControl.xaml.cs
+++ src/RayCarrot.RCP.Metro/Pages/Page_Games/PlayButtonControl.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using MahApps.Metro.Controls;
 
 namespace RayCarrot.RCP.Metro;
 
@@ -16,54 +13,10 @@ public partial class PlayButtonControl : UserControl
         InitializeComponent();
     }
 
-    private void PlayButtonControl_OnLoaded(object sender, RoutedEventArgs e)
-    {
-        // Hacky fix for closing the popup when you scroll with the mouse scroll bar
-        ScrollViewer? parentScrollViewer = this.TryFindParent<ScrollViewer>();
-        if (parentScrollViewer != null)
-            parentScrollViewer.ScrollChanged += (_, _) => DropDownPopup.IsOpen = false;
-
-        // Unsubscribe as the loaded event will trigger each time this tab is selected
-        Loaded -= PlayButtonControl_OnLoaded;
-    }
-
     private void DropDownButton_OnClick(object sender, RoutedEventArgs e)
     {
         // Toggle the state of the popup
         DropDownPopup.IsOpen ^= true;
-    }
-
-    private void DropDownPopup_OnOpened(object sender, EventArgs e)
-    {
-        // Capture the mouse on this so only the button and popup can be interacted with
-        Mouse.Capture(this, CaptureMode.SubTree);
-
-        // Check for mouse clicks outside of this control
-        Mouse.AddPreviewMouseDownOutsideCapturedElementHandler(this, OutsideCapturedElementHandler);
-
-        if (Application.Current.MainWindow != null)
-            Application.Current.MainWindow.Deactivated += MainWindow_OnDeactivated;
-    }
-
-    private void DropDownPopup_OnClosed(object sender, EventArgs e)
-    {
-        // Release the mouse capture
-        ReleaseMouseCapture();
-
-        // Unsubscribe
-        Mouse.RemovePreviewMouseDownOutsideCapturedElementHandler(this, OutsideCapturedElementHandler);
-        if (Application.Current.MainWindow != null)
-            Application.Current.MainWindow.Deactivated -= MainWindow_OnDeactivated;
-    }
-
-    private void MainWindow_OnDeactivated(object sender, EventArgs e)
-    {
-        DropDownPopup.IsOpen = false;
-    }
-
-    private void OutsideCapturedElementHandler(object sender, MouseButtonEventArgs e)
-    {
-        DropDownPopup.IsOpen = false;
     }
 
     private void Button_OnClick(object sender, RoutedEventArgs e)
---
+++ src/RayCarrot.RCP.Metro/UI/Behaviors/DefaultPopupBehavior.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ControlzEx;
+using MahApps.Metro.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace RayCarrot.RCP.Metro;
+
+public class DefaultPopupBehavior : Behavior<PopupEx>
+{
+    private ScrollViewer? _parentScrollViewer;
+
+    public FrameworkElement? ParentElement
+    {
+        get => (FrameworkElement?)GetValue(ParentElementProperty);
+        set => SetValue(ParentElementProperty, value);
+    }
+
+    public static readonly DependencyProperty ParentElementProperty = 
+        DependencyProperty.Register(nameof(ParentElement), typeof(FrameworkElement), typeof(DefaultPopupBehavior));
+
+    protected override void OnAttached()
+    {
+        AssociatedObject.Focusable = false;
+        AssociatedObject.StaysOpen = true;
+        AssociatedObject.AllowsTransparency = true;
+
+        AssociatedObject.Loaded += AssociatedObject_OnLoaded;
+        AssociatedObject.Opened += AssociatedObject_OnOpened;
+        AssociatedObject.Closed += AssociatedObject_OnClosed;
+    }
+
+    protected override void OnDetaching()
+    {
+        if (_parentScrollViewer != null)
+            _parentScrollViewer.ScrollChanged -= ParentScrollViewer_OnScrollChanged;
+
+        _parentScrollViewer = null;
+
+        AssociatedObject.Opened -= AssociatedObject_OnOpened;
+        AssociatedObject.Closed -= AssociatedObject_OnClosed;
+    }
+
+    private void AssociatedObject_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        // Hacky fix for closing the popup when you scroll with the mouse scroll bar
+        _parentScrollViewer = AssociatedObject.TryFindParent<ScrollViewer>();
+        if (_parentScrollViewer != null)
+            _parentScrollViewer.ScrollChanged += ParentScrollViewer_OnScrollChanged;
+
+        AssociatedObject.Loaded -= AssociatedObject_OnLoaded;
+    }
+
+    private void AssociatedObject_OnOpened(object sender, EventArgs e)
+    {
+        if (ParentElement != null)
+        {
+            // Capture the mouse on this so only the button and popup can be interacted with
+            Mouse.Capture(ParentElement, CaptureMode.SubTree);
+
+            // Check for mouse clicks outside of this control
+            Mouse.AddPreviewMouseDownOutsideCapturedElementHandler(ParentElement, OutsideCapturedElementHandler);
+        }
+
+        if (Application.Current.MainWindow != null)
+            Application.Current.MainWindow.Deactivated += MainWindow_OnDeactivated;
+    }
+
+    private void AssociatedObject_OnClosed(object sender, EventArgs e)
+    {
+        // Unsubscribe and release the mouse capture
+        if (ParentElement != null)
+        {
+            Mouse.RemovePreviewMouseDownOutsideCapturedElementHandler(ParentElement, OutsideCapturedElementHandler);
+            ParentElement.ReleaseMouseCapture();
+        }
+
+        if (Application.Current.MainWindow != null)
+            Application.Current.MainWindow.Deactivated -= MainWindow_OnDeactivated;
+    }
+
+    private void ParentScrollViewer_OnScrollChanged(object sender, ScrollChangedEventArgs e)
+    {
+        AssociatedObject.IsOpen = false;
+    }
+
+    private void MainWindow_OnDeactivated(object sender, EventArgs e)
+    {
+        AssociatedObject.IsOpen = false;
+    }
+
+    private void OutsideCapturedElementHandler(object sender, MouseButtonEventArgs e)
+    {
+        AssociatedObject.IsOpen = false;
+    }
+}
