05933d688cc7ed469796dd4cff37a75a892445bf
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-11-28 08:43:59 +0100

    Popup improvements with hacky code

--- src/RayCarrot.RCP.Metro/UI/Behaviors/DefaultPopupBehavior.cs
+++ src/RayCarrot.RCP.Metro/UI/Behaviors/DefaultPopupBehavior.cs
@@ -57,8 +57,21 @@ public class DefaultPopupBehavior : Behavior<PopupEx>
     {
         if (ParentElement != null)
         {
-            // Capture the mouse on this so only the button and popup can be interacted with
+            // Capture the mouse on this so only the parent element (usually the button) and popup can be interacted with
             Mouse.Capture(ParentElement, CaptureMode.SubTree);
+
+            // A bit of a hack. If StaysOpen is false it will close the popup if you press outside of it, which we want.
+            // Just using it however won't allow the parent element to be interacted with since only the popup is captured.
+            // So what we do is first capture it and then set this. It won't work by itself though, so we still need to add
+            // some other checks below... But this will fix an issue where if you click with your mouse inside of the popup
+            // then the capture will be lost and it can't be closed (parent capture will still be lost - but that's fine).
+            //
+            // We might want to get rid of this hack some day. Simplest solution is just to always have StaysOpen set to
+            // false and remove the other code here. Reason I didn't do this is because it doesn't look as good when the
+            // button you clicked to open the popup can't be interacted with after it's opened.
+            // We can also try always keeping StaysOpen to true and keep the other code in here, but then clicking inside
+            // the popup (like on a RadioButton) will break the closing functionality.
+            AssociatedObject.StaysOpen = false;
 
             // Check for mouse clicks outside of this control
             Mouse.AddPreviewMouseDownOutsideCapturedElementHandler(ParentElement, OutsideCapturedElementHandler);
@@ -70,6 +83,8 @@ public class DefaultPopupBehavior : Behavior<PopupEx>
 
     private void AssociatedObject_OnClosed(object sender, EventArgs e)
     {
+        AssociatedObject.StaysOpen = true;
+
         // Unsubscribe and release the mouse capture
         if (ParentElement != null)
         {
