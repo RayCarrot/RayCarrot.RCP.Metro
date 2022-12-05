d298bd35507b0687aa0eaad9c89b0f16878a3700
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-12-05 16:20:07 +0100

    Move Win32 game options ui to user control

---
+++ src/RayCarrot.RCP.Metro/Games/Options/Win32GameOptionsControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RayCarrot.RCP.Metro.Games.Options
+{
+    /// <summary>
+    /// Interaction logic for Win32GameOptionsControl.xaml
+    /// </summary>
+    public partial class Win32GameOptionsControl : UserControl
+    {
+        public Win32GameOptionsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
