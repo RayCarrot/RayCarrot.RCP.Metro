dc9cf6bb56956c8bd96e9572bd2ddb48f2ec9610
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-01-16 15:18:27 +0100

    Rewrite GameIcon control, use ClipBorder

--- src/RayCarrot.RCP.Metro/UI/Controls/GameIcon/GameIcon.cs
+++ src/RayCarrot.RCP.Metro/UI/Controls/GameIcon/GameIcon.cs
@@ -6,6 +6,8 @@ namespace RayCarrot.RCP.Metro;
 
 public class GameIcon : Control
 {
+    #region Source
+
     public ImageSource Source
     {
         get => (ImageSource)GetValue(SourceProperty);
@@ -15,6 +17,10 @@ public class GameIcon : Control
     public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
         nameof(Source), typeof(ImageSource), typeof(GameIcon), new PropertyMetadata(default(ImageSource)));
 
+    #endregion
+
+    #region IconSize
+
     public GameIconSize IconSize
     {
         get => (GameIconSize)GetValue(IconSizeProperty);
@@ -22,7 +28,39 @@ public class GameIcon : Control
     }
 
     public static readonly DependencyProperty IconSizeProperty = DependencyProperty.Register(
-        nameof(IconSize), typeof(GameIconSize), typeof(GameIcon), new PropertyMetadata(GameIconSize.Largest));
+        nameof(IconSize), typeof(GameIconSize), typeof(GameIcon), new PropertyMetadata(GameIconSize.Largest, OnIconSizeChanged));
+
+    private static void OnIconSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        GameIcon gameIcon = (GameIcon)d;
+        GameIconSize iconSize = (GameIconSize)e.NewValue;
+
+        // Set the width and height
+        gameIcon.IconWidth = gameIcon.IconHeight = iconSize switch
+        {
+            GameIconSize.Largest => 128,
+            GameIconSize.Large => 96,
+            GameIconSize.Medium => 64,
+            GameIconSize.Small => 48,
+            GameIconSize.Smallest => 32,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        
+        // Set the corner radius
+        gameIcon.CornerRadius = new CornerRadius(iconSize switch
+        {
+            GameIconSize.Largest => 12,
+            GameIconSize.Large => 9,
+            GameIconSize.Medium => 6,
+            GameIconSize.Small => 4,
+            GameIconSize.Smallest => 2,
+            _ => throw new ArgumentOutOfRangeException()
+        });
+    }
+
+    #endregion
+
+    #region IsDemo
 
     public bool IsDemo
     {
@@ -32,6 +70,53 @@ public class GameIcon : Control
 
     public static readonly DependencyProperty IsDemoProperty = DependencyProperty.Register(
         nameof(IsDemo), typeof(bool), typeof(GameIcon), new PropertyMetadata(false));
+
+    #endregion
+
+    #region IconWidth
+
+    public double IconWidth
+    {
+        get => (double)GetValue(IconWidthProperty);
+        private set => SetValue(IconWidthPropertyKey, value);
+    }
+
+    private static readonly DependencyPropertyKey IconWidthPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(IconWidth), typeof(double), typeof(GameIcon), new FrameworkPropertyMetadata(128d));
+
+    public static readonly DependencyProperty IconWidthProperty = IconWidthPropertyKey.DependencyProperty;
+
+    #endregion
+
+    #region IconHeight
+
+    public double IconHeight
+    {
+        get => (double)GetValue(IconHeightProperty);
+        private set => SetValue(IconHeightPropertyKey, value);
+    }
+
+    private static readonly DependencyPropertyKey IconHeightPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(IconHeight), typeof(double), typeof(GameIcon), new FrameworkPropertyMetadata(128d));
+
+    public static readonly DependencyProperty IconHeightProperty = IconHeightPropertyKey.DependencyProperty;
+
+    #endregion
+
+    #region CornerRadius
+
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        private set => SetValue(CornerRadiusPropertyKey, value);
+    }
+
+    private static readonly DependencyPropertyKey CornerRadiusPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(CornerRadius), typeof(CornerRadius), typeof(GameIcon), new FrameworkPropertyMetadata(new CornerRadius(12d)));
+
+    public static readonly DependencyProperty CornerRadiusProperty = CornerRadiusPropertyKey.DependencyProperty;
+
+    #endregion
 
     public enum GameIconSize
     {
