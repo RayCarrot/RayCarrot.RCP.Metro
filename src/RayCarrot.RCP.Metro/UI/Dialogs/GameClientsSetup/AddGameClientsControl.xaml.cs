1b052c9b91125dcaf52a6a2ae06035a89da63eff
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-07-22 17:03:46 +0200

    Move adding game client to the right column card

---
+++ src/RayCarrot.RCP.Metro/UI/Dialogs/GameClientsSetup/AddGameClientsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RayCarrot.RCP.Metro
+{
+    /// <summary>
+    /// Interaction logic for AddGameClientsControl.xaml
+    /// </summary>
+    public partial class AddGameClientsControl : UserControl
+    {
+        public AddGameClientsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/RayCarrot.RCP.Metro/UI/Dialogs/GameClientsSetup/GameClientHubControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RayCarrot.RCP.Metro
+{
+    /// <summary>
+    /// Interaction logic for GameClientHubControl.xaml
+    /// </summary>
+    public partial class GameClientHubControl : UserControl
+    {
+        public GameClientHubControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
