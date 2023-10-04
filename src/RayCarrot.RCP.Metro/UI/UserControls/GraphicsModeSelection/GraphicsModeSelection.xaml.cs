8827fbbede7dc9067c8e2327dcfd60166b1e97ba
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-10-04 21:32:51 +0200

    Hacky fix for resolution selection in Hebrew

--- src/RayCarrot.RCP.Metro/UI/UserControls/GraphicsModeSelection/GraphicsModeSelection.xaml.cs
+++ src/RayCarrot.RCP.Metro/UI/UserControls/GraphicsModeSelection/GraphicsModeSelection.xaml.cs
@@ -1,4 +1,5 @@
 ﻿#nullable disable
+using System.Windows;
 using System.Windows.Controls;
 
 namespace RayCarrot.RCP.Metro;
@@ -11,5 +12,9 @@ public partial class GraphicsModeSelection : UserControl
     public GraphicsModeSelection()
     {
         InitializeComponent();
+
+        // Hack to force left-to-right for textbox portion of the combobox
+        ModesComboBox.ApplyTemplate();
+        ((TextBox)ModesComboBox.Template.FindName("PART_EditableTextBox", ModesComboBox)).FlowDirection = FlowDirection.LeftToRight;
     }
 }
