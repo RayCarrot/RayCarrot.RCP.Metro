36de77c6ebb9fc3a20e64eff9a7ad85aa0b2f96a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-03-17 13:36:44 +0100

    Add int slider editor field

---
+++ src/RayCarrot.RCP.Metro/UI/EditorFields/ViewModels/EditorIntSliderFieldViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RayCarrot.RCP.Metro;
+
+public class EditorIntSliderFieldViewModel : EditorIntFieldViewModel
+{
+    public EditorIntSliderFieldViewModel(
+        LocalizedString header, 
+        LocalizedString? info, 
+        Func<long> getValueAction, 
+        Action<long> setValueAction,
+        Func<long>? getMinAction = null,
+        Func<long>? getMaxAction = null) 
+        : base(header, info, getValueAction, setValueAction, getMinAction, getMaxAction)
+    { }
+}
