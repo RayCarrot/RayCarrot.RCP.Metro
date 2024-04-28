268e81b40c69101114fb3de802b648996b40428b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-04-28 11:58:14 +0200

    Version 14.1.0.3

--- src/RayCarrot.RCP.Metro/App/AppViewModel.cs
+++ src/RayCarrot.RCP.Metro/App/AppViewModel.cs
@@ -160,7 +160,7 @@ public class AppViewModel : BaseViewModel
     /// </summary>
     public static WindowsVersion WindowsVersion { get; } // TODO: Why is this static?
 
-    public static Version AppVersion => new(14, 1, 0, 2);
+    public static Version AppVersion => new(14, 1, 0, 3);
 
     #endregion
 
