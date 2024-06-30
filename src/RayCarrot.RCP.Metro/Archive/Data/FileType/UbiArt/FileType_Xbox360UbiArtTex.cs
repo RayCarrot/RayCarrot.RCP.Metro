ab340db4f63ecd4a5799faa53078a042bde38b4b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-06-30 14:17:06 +0200

    Update SharpCompress, support uncompressed Xbox 360 textures, minor fixes

--- src/RayCarrot.RCP.Metro/Archive/Data/FileType/UbiArt/FileType_Xbox360UbiArtTex.cs
+++ src/RayCarrot.RCP.Metro/Archive/Data/FileType/UbiArt/FileType_Xbox360UbiArtTex.cs
@@ -48,8 +48,10 @@ public class FileType_Xbox360UbiArtTex : FileType_BaseUbiArtTex
             x.Pre_FileSize = inputStream.Stream.Length;
         });
 
+        bool isCompressed = tex.Xbox360_D3DTexture.DataFormat != D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_8_8_8_8;
+
         // Get the untiled image data
-        byte[] untiledImgData = tex.Xbox360_D3DTexture.Untile(tex.RawData, true);
+        byte[] imgData = tex.Xbox360_D3DTexture.Untile(tex.RawData, swapBytes: isCompressed);
 
         DDSParser.DDSStruct header = new()
         {
@@ -62,13 +64,38 @@ public class FileType_Xbox360UbiArtTex : FileType_BaseUbiArtTex
             depth = 1
         };
 
-        byte[] rawImgData = tex.Xbox360_D3DTexture.DataFormat switch
-        { 
-            D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT1 => DDSParser.DecompressDXT1(header, untiledImgData),
-            D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT2_3 => DDSParser.DecompressDXT3(header, untiledImgData),
-            D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT4_5 => DDSParser.DecompressDXT5(header, untiledImgData),
-            _ => throw new ArgumentOutOfRangeException(nameof(tex.Xbox360_D3DTexture.DataFormat), tex.Xbox360_D3DTexture.DataFormat, null)
-        };
+        // Decompress
+        if (isCompressed)
+        {
+            imgData = tex.Xbox360_D3DTexture.DataFormat switch
+            {
+                D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT1 => DDSParser.DecompressDXT1(header, imgData),
+                D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT2_3 => DDSParser.DecompressDXT3(header, imgData),
+                D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT4_5 => DDSParser.DecompressDXT5(header, imgData),
+                _ => throw new ArgumentOutOfRangeException(nameof(tex.Xbox360_D3DTexture.DataFormat), tex.Xbox360_D3DTexture.DataFormat, null)
+            };
+        }
+
+        // TODO: Include mipmaps in image data - if user exports as DDS they can be included in the export
+        // Remove mipmaps
+        Array.Resize(ref imgData, tex.Xbox360_D3DTexture.ActualWidth * tex.Xbox360_D3DTexture.ActualHeight * 4);
+
+        // Convert ARGB to RGBA
+        if (tex.Xbox360_D3DTexture.DataFormat == D3DTexture.GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_8_8_8_8)
+        {
+            for (int i = 0; i < imgData.Length; i += 4)
+            {
+                byte b0 = imgData[i + 0];
+                byte b1 = imgData[i + 1];
+                byte b2 = imgData[i + 2];
+                byte b3 = imgData[i + 3];
+
+                imgData[i + 0] = b1;
+                imgData[i + 1] = b2;
+                imgData[i + 2] = b3;
+                imgData[i + 3] = b0;
+            }
+        }
 
         // TODO: Do this for other UbiArt platforms too?
         // Only remap if it's not using normal color mapping
@@ -78,22 +105,22 @@ public class FileType_Xbox360UbiArtTex : FileType_BaseUbiArtTex
             colors[4] = 0x00;
             colors[5] = 0xFF;
 
-            for (int i = 0; i < rawImgData.Length; i += 4)
+            for (int i = 0; i < imgData.Length; i += 4)
             {
-                colors[0] = rawImgData[i + 3]; // Alpha
-                colors[1] = rawImgData[i + 0]; // Red
-                colors[2] = rawImgData[i + 1]; // Green
-                colors[3] = rawImgData[i + 2]; // Blue
+                colors[0] = imgData[i + 3]; // Alpha
+                colors[1] = imgData[i + 0]; // Red
+                colors[2] = imgData[i + 1]; // Green
+                colors[3] = imgData[i + 2]; // Blue
 
-                rawImgData[i + 3] = colors[tex.Remap_A];
-                rawImgData[i + 0] = colors[tex.Remap_R];
-                rawImgData[i + 1] = colors[tex.Remap_G];
-                rawImgData[i + 2] = colors[tex.Remap_B];
+                imgData[i + 3] = colors[tex.Remap_A];
+                imgData[i + 0] = colors[tex.Remap_R];
+                imgData[i + 1] = colors[tex.Remap_G];
+                imgData[i + 2] = colors[tex.Remap_B];
             }
         }
 
         // Return the image
-        return new MagickImage(rawImgData, new MagickReadSettings()
+        return new MagickImage(imgData, new MagickReadSettings()
         {
             Format = MagickFormat.Rgba,
             Width = tex.Xbox360_D3DTexture.ActualWidth,
