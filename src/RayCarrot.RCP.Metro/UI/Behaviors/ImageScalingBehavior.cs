96368a8f3525942d19dd020196dc607de4f7f53f
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-08-06 11:54:38 +0200

    Set thumb scaling mode based on if it downscales or upscales

---
+++ src/RayCarrot.RCP.Metro/UI/Behaviors/ImageScalingBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace RayCarrot.RCP.Metro;
+
+public class ImageScalingBehavior : Behavior<Image>
+{
+    public BitmapScalingMode DownscaleScalingMode { get; set; }
+    public BitmapScalingMode UpscaleScalingMode { get; set; }
+
+    private void AssociatedObject_SourceChanged(object sender, EventArgs e)
+    {
+        UpdateScalingMode();
+    }
+
+    private void UpdateScalingMode()
+    {
+        if (AssociatedObject.Source == null)
+            return;
+
+        if (AssociatedObject.Source.Width > AssociatedObject.Width)
+            RenderOptions.SetBitmapScalingMode(AssociatedObject, DownscaleScalingMode);
+        else if (AssociatedObject.Source.Width < AssociatedObject.Width)
+            RenderOptions.SetBitmapScalingMode(AssociatedObject, UpscaleScalingMode);
+        else
+            RenderOptions.SetBitmapScalingMode(AssociatedObject, BitmapScalingMode.Unspecified);
+    }
+
+    protected override void OnAttached()
+    {
+        DependencyPropertyDescriptor descr = DependencyPropertyDescriptor.FromProperty(Image.SourceProperty, typeof(Image));
+        descr.AddValueChanged(AssociatedObject, AssociatedObject_SourceChanged);
+        UpdateScalingMode();
+    }
+
+    protected override void OnDetaching()
+    {
+        DependencyPropertyDescriptor descr = DependencyPropertyDescriptor.FromProperty(Image.SourceProperty, typeof(Image));
+        descr.RemoveValueChanged(AssociatedObject, AssociatedObject_SourceChanged);
+    }
+}
