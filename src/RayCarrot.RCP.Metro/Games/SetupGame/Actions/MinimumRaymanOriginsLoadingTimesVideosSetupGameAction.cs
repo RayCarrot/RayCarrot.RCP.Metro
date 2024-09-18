fbfcf1df59880c2f01a966460560f9ed26dd2989
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-09-18 15:06:39 +0200

    Add setup game action to install Minimum Loading Times mod for Origins

---
+++ src/RayCarrot.RCP.Metro/Games/SetupGame/Actions/MinimumRaymanOriginsLoadingTimesVideosSetupGameAction.cs
@@ -0,0 +1,13 @@
+﻿namespace RayCarrot.RCP.Metro.Games.SetupGame;
+
+public class MinimumRaymanOriginsLoadingTimesVideosSetupGameAction : InstallModSetupGameAction
+{
+    protected override long GameBananaModId => 510228;
+    protected override string ModId => "RaymanOrigins.MinLoading";
+
+    // TODO-LOC
+    public override LocalizedString Header => "Install the Minimum Loading Times mod";
+    public override LocalizedString Info => "The Minimum Loading Times mod is a mod by RayCarrot which changes the minimum loading screen time from 4 seconds to 0 seconds, thus making loading times faster.";
+
+    public override SetupGameActionType Type => SetupGameActionType.Optional;
+}
