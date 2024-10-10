3df32d0e0cc9fd219e3667212ed1a90defe60baf
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-10-10 10:45:36 +0200

    Notify users about TPLS having to be re-installed, delete old utilities data folder

--- src/RayCarrot.RCP.Metro/AppData/AppDataManager.cs
+++ src/RayCarrot.RCP.Metro/AppData/AppDataManager.cs
@@ -756,7 +756,8 @@ public class AppDataManager
 
         if (lastVersion < new Version(14, 2, 1, 0))
         {
-            // Uninstall legacy TPLS data
+            // Uninstall legacy TPLS (per-level soundtrack) data
+            bool uninstalledTpls = false;
             foreach (GameInstallation gameInstallation in GamesManager.GetInstalledGames())
             {
                 LegacyRayman1TplsData? legacyTplsData = gameInstallation.GetObject<LegacyRayman1TplsData>(GameDataKey.R1_LegacyTplsData);
@@ -781,7 +782,25 @@ public class AppDataManager
                 if (legacyTplsData.GameClientInstallationId != null)
                     await Services.GameClients.RemoveGameClientAsync(legacyTplsData.GameClientInstallationId);
 
+                uninstalledTpls = true;
+
                 Logger.Info("Legacy TPLS has been uninstalled for {0}", gameInstallation.FullId);
+            }
+
+            // Notify user about TPLS
+            if (uninstalledTpls)
+            {
+                await Services.MessageUI.DisplayMessageAsync("The Per-level Soundtrack utility for Rayman 1 has been reworked in this version to no longer rely on a custom DOSBox build. To continue using it you have to install it again. The previous version has automatically been uninstalled.", "Per-level Soundtrack Update Notice", MessageType.Information);
+            }
+
+            // Delete the utilities folder since it's no longer used
+            try
+            {
+                Services.File.DeleteDirectory(AppFilePaths.UserDataBaseDir + "Utilities");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Deleting the legacy utilities folder");
             }
         }
     }
