9ef06004029debf661ef000f1051407b82d84807
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-10-27 11:27:17 +0100

    Scroll up when refreshing downloadable mods

--- src/RayCarrot.RCP.Metro/ModLoader/Dialogs/ModLoader/ModLoaderDownloadPageControl.xaml.cs
+++ src/RayCarrot.RCP.Metro/ModLoader/Dialogs/ModLoader/ModLoaderDownloadPageControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -48,5 +49,10 @@ public partial class ModLoaderDownloadPageControl : UserControl
         {
             await ViewModel.LoadNextChunkAsync();
         }
+    }
+
+    private void RefreshButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        ModsScrollViewer.ScrollToTop();
     }
 }
