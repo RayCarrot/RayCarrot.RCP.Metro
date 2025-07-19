aa85b1c238e703ef2a5a3c5e9a1ab36d82713961
Cloaked Thargoid <cloakedthargoid@gmail.com>
2025-07-19 23:33:08 +0300

    Create RRR2/RRR2_SaveFile.cs

---
+++ src/RayCarrot.RCP.Metro/Binary/RRR2/RRR2_SaveFile.cs
@@ -0,0 +1,11 @@
+#nullable disable
+using BinarySerializer;
+
+namespace RayCarrot.RCP.Metro;
+
+public class RRR2_SaveFile : BinarySerializable
+{
+    public override void SerializeImpl(SerializerObject s)
+    {
+    }
+}
