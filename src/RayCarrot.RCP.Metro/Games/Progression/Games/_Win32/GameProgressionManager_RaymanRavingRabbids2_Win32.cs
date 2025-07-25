0b272613c662ecb77dc808f09961c849944526fa
Cloaked Thargoid <cloakedthargoid@gmail.com>
2025-07-25 11:41:42 +0300

    Implement support for individual RRR2 editions save files

--- src/RayCarrot.RCP.Metro/Games/Progression/Games/_Win32/GameProgressionManager_RaymanRavingRabbids2_Win32.cs
+++ src/RayCarrot.RCP.Metro/Games/Progression/Games/_Win32/GameProgressionManager_RaymanRavingRabbids2_Win32.cs
@@ -17,24 +17,30 @@ public class GameProgressionManager_RaymanRavingRabbids2_Win32 : GameProgression
     };
     public override async IAsyncEnumerable<GameProgressionSlot> LoadSlotsAsync(FileSystemWrapper fileSystem)
     {
-        FileSystemPath saveFile = fileSystem.GetFile(Environment.SpecialFolder.MyDocuments.GetFolderPath() + "RRR2" + "RRR2.sav");
-
-        Logger.Info("{0} save is being loaded...", GameInstallation.FullId);
-
-        using RCPContext context = new(saveFile.Parent);
-
-        RRR2_SaveFile? saveData = await context.ReadFileDataAsync<RRR2_SaveFile>(saveFile.Name, new RRR_SaveEncoder(), removeFileWhenComplete: false);
-
-        if (saveData == null)
+        FileSystemPath? saveDir = fileSystem.GetFile(Environment.SpecialFolder.MyDocuments.GetFolderPath() + "RRR2");
+        if (saveDir == null)
         {
-            Logger.Info("{0} save was not found", GameInstallation.FullId);
+            Logger.Info("{0} save directory was not found", GameInstallation.FullId);
             yield break;
         }
 
-        Logger.Info("Save has been deserialized");
+        for (int gameIndex = 0; gameIndex < GameClasses.Length; gameIndex++)
+        {
+            FileSystemPath saveFile = fileSystem.GetFile(Environment.SpecialFolder.MyDocuments.GetFolderPath() + "RRR2" + "RRR2.sav");
 
-        int completedLevels = saveData.MiniGames.Count(x => (x.UserHighScore > 0));
-        List<GameProgressionDataItem> progressItems = new()
+            using RCPContext context = new(saveDir);
+
+            RRR2_SaveFile? saveData = await context.ReadFileDataAsync<RRR2_SaveFile>(GameClasses[gameIndex].SaveFileName, new RRR_SaveEncoder(), removeFileWhenComplete: false);
+
+            if (saveData == null)
+            {
+                yield break;
+            }
+
+            Logger.Info("{0} save {1} has been deserialized...", GameInstallation.FullId, GameClasses[gameIndex].SaveFileName);
+
+            int completedLevels = saveData.MiniGames.Count(x => (x.UserHighScore > 0));
+            List<GameProgressionDataItem> progressItems = new()
         {
             new GameProgressionDataItem(
             isPrimaryItem: true,
@@ -44,23 +50,33 @@ public class GameProgressionManager_RaymanRavingRabbids2_Win32 : GameProgression
             max: 16),
         };
 
-        progressItems.AddRange(Enumerable.Range(0, 16).
-                Where(x => saveData.MiniGames[x].UserHighScore > 0).
-                Select(x => new GameProgressionDataItem(
-                    isPrimaryItem: false,
-                    icon: ProgressionIconAsset.RRR_Star,
-                    header: new ResourceLocString($"RRR2_LevelName_{x}"),
-                    value: saveData.MiniGames[x].UserHighScore)));
+            progressItems.AddRange(Enumerable.Range(0, 16).
+                    Where(x => saveData.MiniGames[x].UserHighScore > 0).
+                    Select(x => new GameProgressionDataItem(
+                        isPrimaryItem: false,
+                        icon: ProgressionIconAsset.RRR_Star,
+                        header: new ResourceLocString($"RRR2_LevelName_{x}"),
+                        value: saveData.MiniGames[x].UserHighScore)));
 
-        yield return new SerializableGameProgressionSlot<RRR2_SaveFile>(
-            name: "Allgames",
-            index: 0,
-            collectiblesCount: 0,
-            totalCollectiblesCount: 16,
-            dataItems: progressItems,
-            context: context,
-            serializable: saveData,
-            fileName: saveFile.Name);
-
+            yield return new SerializableGameProgressionSlot<RRR2_SaveFile>(
+                name: GameClasses[gameIndex].GameDescription,
+                index: 0,
+                collectiblesCount: 0,
+                totalCollectiblesCount: 16,
+                dataItems: progressItems,
+                context: context,
+                serializable: saveData,
+                fileName: saveFile.Name);
+        }
     }
+
+    public GameClass[] GameClasses { get; } =
+    {
+        new("RRR2.sav", "Allgames"),
+        new("RRR2_Blue.sav", "Blue"),
+        new("RRR2_Green.sav", "Green"),
+        new("RRR2_Red.sav", "Red"),
+        new("RRR2_Orange.sav", "Orange"),
+    };
+    public record GameClass(string SaveFileName, string GameDescription);
 }
