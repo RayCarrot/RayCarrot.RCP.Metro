ee7463f35be67a1aa1b04af75a3a1b3b6c08452b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-08-20 12:44:18 +0200

    Serialize potentially unused RRR2 save file in case it's not padding

--- src/RayCarrot.RCP.Metro/Binary/RRR2/RRR2_ScoreEntry.cs
+++ src/RayCarrot.RCP.Metro/Binary/RRR2/RRR2_ScoreEntry.cs
@@ -6,13 +6,14 @@ namespace RayCarrot.RCP.Metro;
 public class RRR2_ScoreEntry : BinarySerializable
 {
     public ulong EncodedName { get; set; }
+    public uint Uint_08 { get; set; }
     public int Score { get; set; }
 
     public override void SerializeImpl(SerializerObject s)
     {
         // Name is stored as two 32-bit little-endian integers; for simplicity treat it as a single 64-bit integer
         EncodedName = s.Serialize<ulong>(EncodedName, name: nameof(EncodedName));
-        s.SerializePadding(4);
+        Uint_08 = s.Serialize<uint>(Uint_08, name: nameof(Uint_08));
         Score = s.Serialize<int>(Score, name: nameof(Score));
     }
 }
