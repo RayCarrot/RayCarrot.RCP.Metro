9dc4a8abca28707de0bf0393624a564f6bf1d6c9
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-08-21 17:51:29 +0200

    Back up TT saves from redirected SaveGames folder for Rib's fix

--- src/RayCarrot.RCP.Metro/Games/Progression/Games/_Win32/GameProgressionManager_TonicTrouble_Win32.cs
+++ src/RayCarrot.RCP.Metro/Games/Progression/Games/_Win32/GameProgressionManager_TonicTrouble_Win32.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using Microsoft.WindowsAPICodePack.Shell;
 
 namespace RayCarrot.RCP.Metro;
 
@@ -9,7 +10,12 @@ public class GameProgressionManager_TonicTrouble_Win32 : GameProgressionManager
 
     public override GameBackups_Directory[] BackupDirectories => new GameBackups_Directory[]
     {
+        // Default location
         new(GameInstallation.InstallLocation.Directory + "gamedata" + "SaveGame", SearchOption.AllDirectories, "*", "0", 0),
-        new(GameInstallation.InstallLocation.Directory + "gamedata" + "Options", SearchOption.AllDirectories, "*", "1", 0)
+        new(GameInstallation.InstallLocation.Directory + "gamedata" + "Options", SearchOption.AllDirectories, "*", "1", 0),
+        
+        // Redirected location using Tonic Trouble Fix
+        new(new FileSystemPath(KnownFolders.SavedGames.Path) + "Tonic Trouble" + "SaveGame", SearchOption.AllDirectories, "*", "2", 0),
+        new(new FileSystemPath(KnownFolders.SavedGames.Path) + "Tonic Trouble" + "Options", SearchOption.AllDirectories, "*", "3", 0),
     };
 }
