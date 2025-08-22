5ac70d20abac36e5d6e29ac26bcba26a5c9415b4
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-08-22 15:48:39 +0200

    Fix converter being declared incorrectly

--- src/RayCarrot.RCP.Metro/UI/Converters/Visibility/InvertedBooleanToVisibilityHiddenConverter.cs
+++ src/RayCarrot.RCP.Metro/UI/Converters/Visibility/InvertedBooleanToVisibilityHiddenConverter.cs
@@ -6,7 +6,7 @@ namespace RayCarrot.RCP.Metro;
 /// <summary>
 /// Converts a <see cref="Boolean"/> to an inverted <see cref="Visibility"/>
 /// </summary>
-public class InvertedBooleanToVisibilityHiddenConverter : BaseValueConverter<InvertedBooleanToVisibilityConverter, bool, Visibility>
+public class InvertedBooleanToVisibilityHiddenConverter : BaseValueConverter<InvertedBooleanToVisibilityHiddenConverter, bool, Visibility>
 {
     public override Visibility ConvertValue(bool value, Type targetType, object parameter, CultureInfo culture) => 
         value ? Visibility.Hidden : Visibility.Visible;
