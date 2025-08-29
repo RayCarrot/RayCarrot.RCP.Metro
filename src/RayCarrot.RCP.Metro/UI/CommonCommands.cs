7ee64cc91fc4b00aa4f1b8b8872dd858cdb62d40
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-08-29 18:59:43 +0200

    Catch exception for copying text to clipboard

--- src/RayCarrot.RCP.Metro/UI/CommonCommands.cs
+++ src/RayCarrot.RCP.Metro/UI/CommonCommands.cs
@@ -5,12 +5,21 @@ namespace RayCarrot.RCP.Metro;
 
 public static class CommonCommands
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
     private static ICommand? _copyToClipboardCommand;
     public static ICommand CopyToClipboardCommand => _copyToClipboardCommand ??= new RelayCommand(x =>
     {
         if (x == null)
             return;
 
-        Clipboard.SetText(x.ToString());
+        try
+        {
+            Clipboard.SetText(x.ToString());
+        }
+        catch (Exception ex)
+        {
+            Logger.Warn(ex, "Copying text to clipboard");
+        }
     });
 }
