eb7730aed974ab475f0765e6de508d0536aadffc
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-02-06 13:35:14 +0100

    Add Rayman 1 Jaguar

---
+++ src/RayCarrot.RCP.Metro/Games/Descriptors/Jaguar/GameDescriptor_Rayman1_Jaguar.cs
@@ -0,0 +1,48 @@
+﻿using RayCarrot.RCP.Metro.Games.Components;
+using RayCarrot.RCP.Metro.Games.Structure;
+
+namespace RayCarrot.RCP.Metro;
+
+/// <summary>
+/// The Rayman 1 (Jaguar) game descriptor
+/// </summary>
+public sealed class GameDescriptor_Rayman1_Jaguar : JaguarGameDescriptor
+{
+    #region Public Properties
+
+    public override string GameId => "Rayman1_Jaguar";
+    public override Game Game => Game.Rayman1;
+    public override GameCategory Category => GameCategory.Rayman;
+
+    public override LocalizedString DisplayName => "Rayman"; // TODO-LOC
+    public override string[] SearchKeywords => new[] { "r1", "jag" };
+    public override DateTime ReleaseDate => new(1995, 09, 09);
+
+    public override GameIconAsset Icon => GameIconAsset.Rayman1;
+    public override GameBannerAsset Banner => GameBannerAsset.Rayman1;
+
+    #endregion
+
+    #region Protected Methods
+
+    protected override void RegisterComponents(IGameComponentBuilder builder)
+    {
+        base.RegisterComponents(builder);
+
+        builder.Register(new ProgressionManagersComponent(x => new GameProgressionManager_Rayman_Jaguar(x, "Rayman 1 Jaguar")));
+        builder.Register<BinaryGameModeComponent>(new Ray1GameModeComponent(Ray1GameMode.Rayman1_Jaguar));
+    }
+
+    protected override ProgramInstallationStructure CreateStructure() => new JaguarRomProgramInstallationStructure();
+
+    #endregion
+
+    #region Public Methods
+
+    public override IEnumerable<GameAddAction> GetAddActions() => new GameAddAction[]
+    {
+        new LocateFileGameAddAction(this),
+    };
+
+    #endregion
+}
---
+++ src/RayCarrot.RCP.Metro/Games/Progression/Games/Jaguar/GameProgressionManager_Rayman_Jaguar.cs
@@ -0,0 +1,103 @@
+﻿using BinarySerializer.Ray1.Jaguar;
+
+namespace RayCarrot.RCP.Metro;
+
+public class GameProgressionManager_Rayman_Jaguar : EmulatedGameProgressionManager
+{
+    public GameProgressionManager_Rayman_Jaguar(GameInstallation gameInstallation, string progressionId) 
+        : base(gameInstallation, progressionId) 
+    { }
+
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public override async IAsyncEnumerable<EmulatedGameProgressionSlot> LoadSlotsAsync(EmulatedSave emulatedSave)
+    {
+        JAG_SaveData saveData = await emulatedSave.ReadAsync<JAG_SaveData>();
+
+        for (int saveIndex = 0; saveIndex < saveData.SaveSlots.Length; saveIndex++)
+        {
+            JAG_SaveSlot saveSlot = saveData.SaveSlots[saveIndex];
+
+            if (saveSlot.SaveName.IsNullOrEmpty())
+                continue;
+
+            int cages = 0;
+
+            void addCages(JAG_SaveLevel saveLevel)
+            {
+                if (saveLevel.Cage1)
+                    cages++;
+                if (saveLevel.Cage2)
+                    cages++;
+                if (saveLevel.Cage3)
+                    cages++;
+                if (saveLevel.Cage4)
+                    cages++;
+                if (saveLevel.Cage5)
+                    cages++;
+                if (saveLevel.Cage6)
+                    cages++;
+            }
+
+            // Calculate cages from each level
+            addCages(saveSlot.PinkPlantWoods);
+            addCages(saveSlot.AnguishLagoon);
+            addCages(saveSlot.ForgottenSwamps);
+            addCages(saveSlot.MoskitosNest);
+            addCages(saveSlot.BongoHills);
+            addCages(saveSlot.AllegroPresto);
+            addCages(saveSlot.GongHeights);
+            addCages(saveSlot.MrSaxsHullaballoo);
+            addCages(saveSlot.TwilightGulch);
+            addCages(saveSlot.TheHardRocks);
+            addCages(saveSlot.MrStonesPeaks);
+            addCages(saveSlot.EraserPlains);
+            addCages(saveSlot.PencilPentathlon);
+            addCages(saveSlot.SpaceMamasCrater);
+            addCages(saveSlot.CrystalPalace);
+            addCages(saveSlot.EatatJoes);
+            addCages(saveSlot.MrSkopsStalactites);
+
+            // Convert from BCD
+            int livesCount = 0;
+            livesCount += 10 * (saveSlot.LivesCount >> 4);
+            livesCount += saveSlot.LivesCount & 0xF;
+
+            int slotIndex = saveIndex;
+
+            yield return new SerializabeEmulatedGameProgressionSlot<JAG_SaveData>(
+                name: saveSlot.SaveName.ToUpper(),
+                index: saveIndex,
+                collectiblesCount: cages,
+                totalCollectiblesCount: 102,
+                emulatedSave: emulatedSave,
+                dataItems: new[]
+                {
+                    new GameProgressionDataItem(
+                        isPrimaryItem: true,
+                        icon: ProgressionIconAsset.R1_Cage,
+                        header: new ResourceLocString(nameof(Resources.Progression_Cages)),
+                        value: cages,
+                        max: 102),
+                    new GameProgressionDataItem(
+                        isPrimaryItem: false,
+                        icon: ProgressionIconAsset.R1_Continue,
+                        header: new ResourceLocString(nameof(Resources.Progression_Continues)),
+                        value: saveSlot.ContinuesCount),
+                    new GameProgressionDataItem(
+                        isPrimaryItem: false,
+                        icon: ProgressionIconAsset.R1_Life,
+                        header: new ResourceLocString(nameof(Resources.Progression_Lives)),
+                        value: livesCount),
+                },
+                serializable: saveData)
+            {
+                GetExportObject = x => x.SaveSlots[slotIndex],
+                SetImportObject = (x, o) => x.SaveSlots[slotIndex] = (JAG_SaveSlot)o,
+                ExportedType = typeof(JAG_SaveSlot)
+            };
+        }
+
+        Logger.Info("{0} save has been loaded", GameInstallation.FullId);
+    }
+}
