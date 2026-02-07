2f8947858c7a736e48fc05543f59c080cc554dae
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-02-07 08:51:35 +0100

    Add Rayman 3 and Arena GameCube

---
+++ src/RayCarrot.RCP.Metro/Games/Descriptors/GameCube/GameDescriptor_Rayman3_GameCube.cs
@@ -0,0 +1,51 @@
+﻿using RayCarrot.RCP.Metro.Games.Components;
+using RayCarrot.RCP.Metro.Games.Structure;
+
+namespace RayCarrot.RCP.Metro;
+
+/// <summary>
+/// The Rayman 3 (GameCube) game descriptor
+/// </summary>
+public sealed class GameDescriptor_Rayman3_GameCube : GameCubeGameDescriptor
+{
+    #region Public Properties
+
+    public override string GameId => "Rayman3_GameCube";
+    public override Game Game => Game.Rayman3;
+    public override GameCategory Category => GameCategory.Rayman;
+
+    public override LocalizedString DisplayName => "Rayman 3"; // TODO-LOC
+    public override string[] SearchKeywords => new[] { "r3" };
+    public override DateTime ReleaseDate => new(2003, 02, 21);
+
+    public override GameIconAsset Icon => GameIconAsset.Rayman3;
+    public override GameBannerAsset Banner => GameBannerAsset.Rayman3;
+
+    #endregion
+
+    #region Protected Methods
+
+    protected override void RegisterComponents(IGameComponentBuilder builder)
+    {
+        base.RegisterComponents(builder);
+
+        builder.Register<BinaryGameModeComponent>(new CPAGameModeComponent(CPAGameMode.Rayman3_GC));
+    }
+
+    protected override ProgramInstallationStructure CreateStructure() => new GameCubeDiscProgramInstallationStructure(new[]
+    {
+        new GameCubeProgramLayout("EU", "RAYMAN 3 HOODLUM HAVOC", "GRHP", "41"),
+        new GameCubeProgramLayout("US", "RAYMAN 3 HOODLUM HAVOC", "GRHE", "41"),
+    });
+
+    #endregion
+
+    #region Public Methods
+
+    public override IEnumerable<GameAddAction> GetAddActions() => new GameAddAction[]
+    {
+        new LocateFileGameAddAction(this),
+    };
+
+    #endregion
+}
---
+++ src/RayCarrot.RCP.Metro/Games/Descriptors/GameCube/GameDescriptor_RaymanArena_GameCube.cs
@@ -0,0 +1,50 @@
+﻿using RayCarrot.RCP.Metro.Games.Components;
+using RayCarrot.RCP.Metro.Games.Structure;
+
+namespace RayCarrot.RCP.Metro;
+
+/// <summary>
+/// The Rayman Arena (GameCube) game descriptor
+/// </summary>
+public sealed class GameDescriptor_RaymanArena_GameCube : GameCubeGameDescriptor
+{
+    #region Public Properties
+
+    public override string GameId => "RaymanArena_GameCube";
+    public override Game Game => Game.RaymanMArena;
+    public override GameCategory Category => GameCategory.Rayman;
+
+    public override LocalizedString DisplayName => "Rayman Arena"; // TODO-LOC
+    public override string[] SearchKeywords => new[] { "ra" };
+    public override DateTime ReleaseDate => new(2002, 09, 24);
+
+    public override GameIconAsset Icon => GameIconAsset.RaymanArena;
+    public override GameBannerAsset Banner => GameBannerAsset.RaymanM;
+
+    #endregion
+
+    #region Protected Methods
+
+    protected override void RegisterComponents(IGameComponentBuilder builder)
+    {
+        base.RegisterComponents(builder);
+
+        builder.Register<BinaryGameModeComponent>(new CPAGameModeComponent(CPAGameMode.RaymanArena_GC));
+    }
+
+    protected override ProgramInstallationStructure CreateStructure() => new GameCubeDiscProgramInstallationStructure(new[]
+    {
+        new GameCubeProgramLayout("US", "RAYMAN ARENA", "GRYE", "41"),
+    });
+
+    #endregion
+
+    #region Public Methods
+
+    public override IEnumerable<GameAddAction> GetAddActions() => new GameAddAction[]
+    {
+        new LocateFileGameAddAction(this),
+    };
+
+    #endregion
+}
